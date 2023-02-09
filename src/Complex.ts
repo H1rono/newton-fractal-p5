524e2c4f5587e4174988bd97ec467fed402c14c4
H1rono <hronok66@gmail.com>
2023-02-09 17:32:05 +0900

    Add Complex.ts

---
+++ src/Complex.ts
@@ -0,0 +1,95 @@
+class Complex {
+    public re: number;
+    public im: number;
+
+    constructor(re: number, im: number) {
+        this.re = re;
+        this.im = im;
+    }
+
+    static fromNumber(n: number): Complex {
+        return new Complex(n, 0);
+    }
+
+    magSq(): number {
+        return this.re * this.re + this.im * this.im;
+    }
+
+    distSq(o: Complex | number): number {
+        let x = typeof o === "number" ? Complex.fromNumber(o) : o;
+        return this.sub(x).magSq();
+    }
+
+    conj(): Complex {
+        return new Complex(this.re, -this.im);
+    }
+
+    conjMut(): Complex {
+        this.im *= -1;
+        return this;
+    }
+
+    neg(): Complex {
+        return new Complex(-this.re, -this.im);
+    }
+
+    negMut(): Complex {
+        this.re *= -1;
+        this.im *= -1;
+        return this;
+    }
+
+    add(o: Complex | number): Complex {
+        let x = typeof o === "number" ? Complex.fromNumber(o) : o;
+        return new Complex(this.re + x.re, this.im + x.im);
+    }
+
+    addMut(o: Complex | number): Complex {
+        let x = typeof o === "number" ? Complex.fromNumber(o) : o;
+        this.re += x.re;
+        this.im += x.im;
+        return this;
+    }
+
+    sub(o: Complex | number): Complex {
+        let x = typeof o === "number" ? Complex.fromNumber(o) : o;
+        return new Complex(this.re - x.re, this.im - x.im);
+    }
+
+    subMut(o: Complex | number): Complex {
+        let x = typeof o === "number" ? Complex.fromNumber(o) : o;
+        this.re -= x.re;
+        this.im -= x.im;
+        return this;
+    }
+
+    mul(o: Complex | number): Complex {
+        let x = typeof o === "number" ? Complex.fromNumber(o) : o;
+        return new Complex(this.re * x.re - this.im * x.im, this.re * x.im + this.im * x.re);
+    }
+
+    mulMut(o: Complex | number): Complex {
+        let x = typeof o === "number" ? Complex.fromNumber(o) : o;
+        this.re = this.re * x.re - this.im * x.im;
+        this.im = this.re * x.im + this.im * x.re;
+        return this;
+    }
+
+    div(o: Complex | number): Complex {
+        let x = typeof o === "number" ? Complex.fromNumber(o) : o;
+        const numer = this.mul(x.conj());
+        const denom = x.magSq();
+        return new Complex(numer.re / denom, numer.im / denom);
+    }
+
+    divMut(o: Complex | number): Complex {
+        let x = typeof o === "number" ? Complex.fromNumber(o) : o;
+        this.mulMut(x.conj());
+        const denom = x.magSq();
+        this.re /= denom;
+        this.im /= denom;
+        return this;
+    }
+}
+
+export default Complex;
